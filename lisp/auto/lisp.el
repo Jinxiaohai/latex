1158189ed4ee511a572b72db54b13527f9115e21
Jinxiaohai <xiaohaiJin@outlook.com>
2017-08-04 21:43:21 +0800

    first commit

---
+++ lisp/auto/lisp.el
@@ -0,0 +1,71 @@
+(TeX-add-style-hook
+ "lisp"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("ctexbook" "12pt" "UTF8" "hyperref" "openany")))
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "chapter1/chapter1"
+    "chapter2/chapter2"
+    "chapter3/chapter3"
+    "chapter4/chapter4"
+    "chapter5/chapter5"
+    "chapter6/chapter6"
+    "chapter7/chapter7"
+    "chapter8/chapter8"
+    "chapter9/chapter9"
+    "chapter10/chapter10"
+    "chapter11/chapter11"
+    "ctexbook"
+    "ctexbook12"
+    "amsmath"
+    "amssymb"
+    "array"
+    "booktabs"
+    "calc"
+    "caption"
+    "ctexcap"
+    "color"
+    "fancyhdr"
+    "graphicx"
+    "geometry"
+    "hyperref"
+    "ifthen"
+    "longtable"
+    "lineno"
+    "listings"
+    "multicol"
+    "makeidx"
+    "ntheorem"
+    "paralist"
+    "tabularx"
+    "titlesec"
+    "fancyvrb"
+    "fontspec"
+    "titletoc"
+    "xcolor"
+    "xeCJK"
+    "wallpaper")
+   (TeX-add-symbols
+    '("mycmdC" 2)
+    '("mycmdB" 1)
+    "mycmdA")))
+
