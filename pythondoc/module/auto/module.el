2b6667c6fb93e3db423267c1c3fe35dc44dd3557
Jinxiaohai <xiaohaiJin@outlook.com>
2017-08-07 22:10:54 +0800

    path

--- pythondoc/module/auto/module.el
+++ pythondoc/module/auto/module.el
@@ -3,24 +3,24 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("ctexbook" "a4paper" "12pt" "UTF8" "hyperref" "openany")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "chapter2/chapter2"
@@ -33,6 +33,31 @@
     "chapter9/chapter9"
     "chapter10/chapter10"
     "chapter11/chapter11"
+    "chapter12/chapter12"
+    "chapter13/chapter13"
+    "chapter14/chapter14"
+    "chapter15/chapter15"
+    "chapter16/chapter16"
+    "chapter17/chapter17"
+    "chapter18/chapter18"
+    "chapter19/chapter19"
+    "chapter20/chapter20"
+    "chapter21/chapter21"
+    "chapter22/chapter22"
+    "chapter23/chapter23"
+    "chapter24/chapter24"
+    "chapter25/chapter25"
+    "chapter26/chapter26"
+    "chapter27/chapter27"
+    "chapter28/chapter28"
+    "chapter29/chapter29"
+    "chapter30/chapter30"
+    "chapter31/chapter31"
+    "chapter32/chapter32"
+    "chapter33/chapter33"
+    "chapter34/chapter34"
+    "chapter35/chapter35"
+    "chapter36/chapter36"
     "ctexbook"
     "ctexbook12"
     "amsmath"
